6200f2f8d4ad839ee9546d1021cad728ecc95595
root <dcreno@gmail.com>
2016-04-05 16:52:26 +0000

    sends find my iphone events

--- hsec-alert/hsec-alert.py
+++ hsec-alert/hsec-alert.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3.4
 """
-Receive events, decide what to do. Based on zguide.
+Receive events and state changes. Alert interested parties.
 """
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 import requests             # for webhooks
 import configparser         # for reading config
-import time
+import time                 # for sleeping
 import comms.comms as comms # for getting a channel to the sensor
+from pyicloud import PyiCloudService # for sending "find my iPhone alerts"
+
+
+def iCloud_alert(account, device_name):
+
+    api=PyiCloudService(account) # password already saved via icloud cmdline
+
+    # look through the list of devices associated with this account
+    for k, v in api.devices.items():
+
+	# find the device that matches what you have configured to receive the alerts
+        if api.devices[k].data['name'] == device_name:
+
+            # send an alert
+            api.devices[k].play_sound("door opened")
 
 
 def main():
@@ -17,33 +32,29 @@ def main():
     config = configparser.ConfigParser()
     config.read('hsec-alert.cfg')
     key=config['maker.ifttt.com']['Key']
+    appleAlertAccount = config['Apple.alert.devices']['Account']
+    appleAlertDevice  = config['Apple.alert.devices']['Device']
 
-    # create object for communication to sensor system
-    comm_channel = comms.SubChannel("tcp://localhost:5564", ['state'])
+    # create object for communication to state system
+    state_channel = comms.SubChannel("tcp://localhost:5564", ['state'])
 
     try:
         while True:
             # Read envelope and address from queue
-            rv = comm_channel.get()
+            rv = state_channel.get()
             if rv is not None:
                 [address, contents] = rv
-                print("state: [%s] %s" % (address, contents))
+                print("State: [%s] %s" % (address, contents))
                 post = "https://maker.ifttt.com/trigger/front_door_opened/with/key/" + key
                 print("not really..." + post)
+                iCloud_alert( appleAlertAccount, appleAlertDevice)
                 #print(requests.post(post))
             else:
                 time.sleep(0.1)
-            #print("doing stuff")
-            #time.sleep(1)
-            # trigger an event
 
     except KeyboardInterrupt:
         pass
-        #q.join(timeout=1) 	# probably goes in comms class
 
-    # clean up zmq connection
-    subscriber.close()
-    context.term()
 
 if __name__ == "__main__":
     main()
